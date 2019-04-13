1304fae5aae87b612682b831162c45def08235b0
StormXTR <38572991+StormXTR@users.noreply.github.com>
2019-04-13 23:47:42 +0300

    Create ascii.js

---
+++ komutlar/ascii.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, msg, args) => {
+    if(!msg.member.hasPermission("MANAGE_MESSAGES")){
+        return msg.channel.send("Bu komutu kullanmak için gerekli izine sahip değilsin!");
+    }
+    else{
+        let mesaj = args.join(" ");
+
+        msg.delete().catch(O_o=>{}); // mesaj yakalama ve silme
+        msg.channel.sendCode("asciidoc", mesaj);
+        return;
+    }
+
+}
+
+module.exports.help = {
+    name: "ascii"
+}
