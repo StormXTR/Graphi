22674fa1fc66d697fa72ae263a72789c3e8d32d6
StormXTR <38572991+StormXTR@users.noreply.github.com>
2019-04-14 00:59:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ client.on("messageDelete", (messageDelete) =>{
     .setTitle(messageDelete.channel.name + " adlı kanaldan " + messageDelete.author.tag + " adlı kişinin mesajı silindi.")
     .setColor("RANDOM")
     .addField("Mesaj İçeriği", messageDelete.content)
-    .addField("Silinme Zamanı", messageDelete.createdAt)
+    .addField("Silinme Zamanı", messageDelete.createdAt.getTime)
     .setFooter(client.user.username + " v1.0" , client.user.avatarURL);
 
     kanal.send(mesajlog);
