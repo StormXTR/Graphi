ad1733543981a6f5588e5cf72b8faae1c1ca1965
StormXTR <38572991+StormXTR@users.noreply.github.com>
2019-04-13 23:47:20 +0300

    Update yazdir.js

--- komutlar/yazdir.js
+++ komutlar/yazdir.js
@@ -1,12 +1,19 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, msg, args) => {
-    let mesaj = args.join(" ");
-    msg.delete().catch(O_o=>{}); // mesaj yakalama ve silme
+    if(!msg.member.hasPermission("MANAGE_MESSAGES")){
+        return msg.channel.send("Bu komutu kullanmak için gerekli izine sahip değilsin!");
+    }
+    else{
+        let mesaj = args.join(" ");
 
-    msg.channel.sendCode("asciidoc", mesaj);
+        msg.delete().catch(O_o=>{}); // mesaj yakalama ve silme
+        msg.channel.send(mesaj);
+        return;
+    }
+
 }
 
 module.exports.help = {
-    name: "yazdır"
+    name: "yazdir"
 }
