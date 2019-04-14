5c17b42779968a4210960c85eb5e3544f08e11ac
StormXTR <38572991+StormXTR@users.noreply.github.com>
2019-04-14 13:31:33 +0300

    Update and rename dmmesaj.js to öner.js

---
+++ komutlar/öner.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, msg, args) => {
+
+    let mesaj = args.join(" ");
+
+    let kisi = client.users.get("485515836707176458");
+    let embed = new Discord.RichEmbed()
+    .setTitle(msg.author.tag + " adlı kişiden bir mesaj")
+    .setThumbnail(msg.authur.avatarURL)
+    .setColor("RANDOM")
+    .addField("Gönderen Kişi ID", msg.author.username, true)
+    .addField("Mesaj İçeriği", mesaj, true)
+    .setTimestamp()
+    .setFooter(client.user.tag, client.user.avatarURL);
+
+    kisi.send(embed);
+
+    msg.delete().catch(O_o=>{});
+
+}
+module.exports.help = {
+    name: "öner"
+}
