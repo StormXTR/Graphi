8598fee26a24a99be05095f126158b42c9045807
StormXTR <38572991+StormXTR@users.noreply.github.com>
2019-04-14 12:56:01 +0300

    Update dmmesaj.js

--- komutlar/dmmesaj.js
+++ komutlar/dmmesaj.js
@@ -2,7 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, msg, args) => {
 
-    msg.member.send("Deneme Mesaj");
+    let mesaj = args.join(" ");
+
+    msg.member.send("Duyuru mesaj"+ mesaj);
+    msg.delete().catch(O_o=>{});
 
 }
 module.exports.help = {
