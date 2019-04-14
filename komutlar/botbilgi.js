a96c5b621d36d696dd1ec49bcffb44e18698aa32
StormXTR <38572991+StormXTR@users.noreply.github.com>
2019-04-14 14:55:21 +0300

    Update botbilgi.js

--- komutlar/botbilgi.js
+++ komutlar/botbilgi.js
@@ -6,7 +6,7 @@ module.exports.run = async (client, msg, args) => {
     .setTitle(client.user.username + " Bot Bilgi")
     .addField("Hizmet verilen sunucu sayısı", client.guilds.size, true)
     .addField("Discord JS Sürümü", "v"+Discord.version, true)
-    .addField("Ping", client.ping + " MS")
+    .addField("Ping", client.ping + " MS",true)
     .setFooter(client.user.username + " v1.0", client.user.avatarURL);
 
     msg.channel.send(embed);
