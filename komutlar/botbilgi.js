7c56892230d5c20b942d9afe462558eda4a6435e
StormXTR <38572991+StormXTR@users.noreply.github.com>
2019-04-17 17:07:10 +0300

    Update botbilgi.js

--- komutlar/botbilgi.js
+++ komutlar/botbilgi.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, msg, args) => {
     let embed = new Discord.RichEmbed()
     .setColor("RANDOM")
     .setTitle(client.user.username + " Bot Bilgi")
-    .addField("Hizmet verilen sunucu sayısı", client.guilds.size, true)
+    .addField("Hizmet verilen sunucu sayısı", client.guilds.size)
     .addField("Discord JS Sürümü", "v"+Discord.version, true)
     .addField("Ping", client.ping + " MS",true)
     .setFooter(client.user.username + " v1.0", client.user.avatarURL);
