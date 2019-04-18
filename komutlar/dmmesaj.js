09f2fdf129da3ddbda4b68498a6b9b2e60b51eb0
StormXTR <38572991+StormXTR@users.noreply.github.com>
2019-04-18 21:13:11 +0300

    Update dmmesaj.js

--- komutlar/dmmesaj.js
+++ komutlar/dmmesaj.js
@@ -2,22 +2,29 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, msg, args) => {
 
-    let mesaj = args.join(" ");
-    if(mesaj == null){
-        return msg.channel.send("Bir mesaj giriniz.");
+    if(!msg.member.hasPermission("ADMINISTRATOR")) {
+        return msg.reply("Bu mesajı sadece yetkililer kullanabilir!");
     }
+    else{
+        let mesaj = args.join(" ");
+        if(mesaj.length < 0){
+            return msg.channel.send("Bir mesaj giriniz.");
+        }
+        if()
 
-    msg.guild.members.forEach(function(uyeler) {
-        let embed = new Discord.RichEmbed()
-        .setTitle("Dm Özel Mesaj")
-        .setColor("RANDOM")
-        .addField("Mesaj İçeriği", mesaj, true)
-        .setFooter(client.user.username, client.user.avatarURL)
-        .setTimestamp();
-        uyeler.user.send(embed);
-    });
-    msg.delete().catch(O_o=>{});
-    msg.reply("Mesajınız Gönderildi!").then(msg => msg.delete(3000));
+        msg.guild.members.forEach(function(uyeler) {
+            let embed = new Discord.RichEmbed()
+            .setTitle("Dm Özel Mesaj")
+            .setColor("RANDOM")
+            .addField("Mesaj İçeriği", mesaj, true)
+            .setFooter(client.user.username, client.user.avatarURL)
+            .setTimestamp();
+            uyeler.user.send(embed);
+        });
+        msg.delete().catch(O_o=>{});
+        msg.reply("Mesajınız Gönderildi!").then(msg => msg.delete(3000));
+    }
+   
 }
 
 module.exports.help = {
