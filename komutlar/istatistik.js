9bb34c6582936dd58d718ce51ecfa51f662ef4cd
StormXTR <38572991+StormXTR@users.noreply.github.com>
2019-04-19 22:06:12 +0300

    Update istatistik.js

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -4,7 +4,6 @@ module.exports.run = async (client, msg, args) => {
     let embed = new Discord.RichEmbed()
         .setAuthor(msg.author.tag + " adlı kişi sunucudurum komutunu çalıştırdı", msg.author.avatarURL)
         .setColor("RANDOM")
-        .setThumbnail("https://cdn.discordapp.com/icons/"+ client.guild.id + "/" + client.guild.icon + ".png")
         .addField("Sunucu Sahibi", client.guild.owner,true)
         .addField("Kişi Sayısı", client.guild.members.size,true)
         .addField("Sunucunun Bulunduğu bölge", client.guild.region,true)
