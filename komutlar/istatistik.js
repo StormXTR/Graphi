7cc591cbcf9db3fd216ef3d5e9791a7df1e33d39
StormXTR <38572991+StormXTR@users.noreply.github.com>
2019-04-19 21:57:35 +0300

    Create istatistik.js

---
+++ komutlar/istatistik.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, msg, args) => {
+    let embed = new Discord.RichEmbed()
+        .setAuthor(msg.author.tag + " adlı kişi sunucudurum komutunu çalıştırdı", msg.author.avatarURL)
+        .setColor("RANDOM")
+        .setThumbnail(client.guild.iconURL)
+        .addField("Sunucu Sahibi", client.guild.owner,true)
+        .addField("Kişi Sayısı", client.guild.members.size,true)
+        .addField("Sunucunun Bulunduğu bölge", client.guild.region,true)
+        .addField("Oluşturulma Tarihi", client.guild.createdAt,true)
+        .setFooter(client.user.tag + " v1.1", client.user.avatarURL);
+    msg.channel.send(embed);
+}
+module.exports.help = {
+    name: "sunucudurumu"
+}
