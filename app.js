ce01b2e6bd851b82b2877649775327e98642f197
StormXTR <38572991+StormXTR@users.noreply.github.com>
2019-04-19 17:16:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ client.commands = new Discord.Collection();
         });
     });
 client.on("ready", () => {
-    console.log(`Bot Giriş Yaptı! ${client.user.tag} ID: ${client.user.id} Sunucu Sayısı: ${client.user.size}`);
+    console.log(`Bot Giriş Yaptı! ${client.user.tag} ID: ${client.user.id} Sunucu Sayısı: ${client.users.size}`);
 });
 
 client.on("messageDelete", (messageDelete) =>{
