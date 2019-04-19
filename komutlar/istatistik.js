fae2304f9c57cf7cfa9a3613dd6054db77de516c
StormXTR <38572991+StormXTR@users.noreply.github.com>
2019-04-19 22:10:49 +0300

    Update istatistik.js

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -1,13 +1,16 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, msg, args) => {
+    let sicon = msg.guild.displayAvatarURL;
     let embed = new Discord.RichEmbed()
         .setAuthor(msg.author.tag + " adlı kişi sunucudurum komutunu çalıştırdı", msg.author.avatarURL)
         .setColor("RANDOM")
-        .addField("Sunucu Sahibi", client.guild.owner,true)
-        .addField("Kişi Sayısı", client.guild.members.size,true)
-        .addField("Sunucunun Bulunduğu bölge", client.guild.region,true)
-        .addField("Oluşturulma Tarihi", client.guild.createdAt,true)
+        .setThumbnail(sicon)
+        .addField("Sunucu Sahibi", msg.guild.owner,true)
+        .addField("Kişi Sayısı", msg.guild.members.size,true)
+        .addField("Kişi Sayısı2", msg.guild.memberCount,true)
+        .addField("Sunucunun Bulunduğu bölge", msg.guild.region,true)
+        .addField("Oluşturulma Tarihi", msg.guild.createdAt,true)
         .setFooter(client.user.tag + " v1.1", client.user.avatarURL);
     msg.channel.send(embed);
 }
